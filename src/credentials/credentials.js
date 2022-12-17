50aaca7676179416eb5d7b517eecef0a0da4bc70
DaanJesus <danilo25oliveira@gmail.com>
2022-12-16 23:33:00 -0300

    new path

--- src/credentials/credentials.js
+++ src/credentials/credentials.js
@@ -4,5 +4,5 @@ module.exports = {
 	sandbox: process.env.SANDBOX,
 	client_id: process.env.CLIENT_ID,
 	client_secret: process.env.CLIENT_SECRET,
-	pix_cert: 'C:/Users/danil/Desktop/site/api-tarot-hekate/src/credentials/prod.p12',
+	pix_cert: process.env.PATH_CERT,
 };
