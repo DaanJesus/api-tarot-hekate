875236d7c16e1e1c58e864d4c2e4f5d1c560202f
DaanJesus <danilo25oliveira@gmail.com>
2022-12-16 23:24:40 -0300

    teste .env

--- src/credentials/credentials.js
+++ src/credentials/credentials.js
@@ -1,8 +1,8 @@
 module.exports = {
 	// PRODUÇÃO = false
 	// HOMOLOGAÇÃO = true
-	sandbox: true,
+	sandbox: process.env.SANDBOX,
 	client_id: process.env.CLIENT_ID,
 	client_secret: process.env.CLIENT_SECRET,
-	pix_cert: 'C:/Users/danil/Desktop/site/api-tarot-hekate/src/credentials/homolog.p12',
+	pix_cert: 'C:/Users/danil/Desktop/site/api-tarot-hekate/src/credentials/prod.p12',
 };
