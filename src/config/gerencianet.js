c4ffacf61c2703e261c313dd8cf2306757707d4f
danilo.jesus <danilo.jesus@grupofleury.com.br>
2023-01-13 19:58:09 -0300

    teste

--- src/config/gerencianet.js
+++ src/config/gerencianet.js
@@ -3,7 +3,7 @@ var fs = require("fs");
 const path = require("path");
 const https = require("https");
 
-const cert = fs.readFileSync(path.resolve(__dirname, `../credentials/homolog.p12`))
+const cert = fs.readFileSync(path.resolve(__dirname, `../credentials/prod.p12`))
 
 const credential = Buffer(
     `${process.env.CLIENT_ID}:${process.env.CLIENT_SECRET}`
